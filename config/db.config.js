6e76d5861bbad471e3bb997da797bed28514b8a2
symplice-nunu <intwarisymplice@gmail.com>
2023-07-19 15:34:54 +0530

    fix: changing database connection

--- config/db.config.js
+++ config/db.config.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const dbConn = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '',
+  password : 'your_new_password',
   database : 'mwe'
 });
 dbConn.connect(function(err) {
